6f0d918d5d8c680b061296acb9452963009b7e41
Aman Kumar <akdapunk@gmail.com>
2018-11-30 12:54:32 +0530

    renamed

--- python/json_prettifier.py
+++ python/json_prettifier.py
@@ -1,7 +1,7 @@
 import json
 
 ugly_json = raw_input()
-parsed = json.loads(ugly_json)
-print(json.dumps(parsed, indent=2, sort_keys=True))
+pretty_json = json.loads(ugly_json)
+print(json.dumps(pretty_json, indent=2, sort_keys=True))
 
 
